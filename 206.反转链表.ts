dd80b5dc66d853311e2e0c9088606bc8b7d8f4e8
Li <1977735808@qq.com>
2022-05-13 17:20:10 +0800

    clock[链表]: 反转链表

---
+++ 206.反转链表.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let curr =head
+  let prev = null
+
+  while (curr) {
+    let next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  return prev
+};
+// @lc code=end
+
